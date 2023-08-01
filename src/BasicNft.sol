6afee65988f437dbf13ac0c4b1cb8e38495997d6
worksofallwin <princeallwin199@gmail.com>
2023-08-01 15:28:10 +0530

    Add img and layout.md

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -3,6 +3,46 @@ pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/// @title Basic NFT
+/// @author Prince Allwin
+/// @notice This contract is creating Dogie NFTs
 contract BasicNft is ERC721 {
-    constructor() ERC721("Basic Nft", "BFT") {}
+    /*/////////////////////////////////////////////////////////////////////////////
+                                PRIVATE STORAGE
+    /////////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev tokenCounter will act as the tokenId, it will be initialized in the constructor
+    uint256 private s_tokenCounter;
+
+    /// @dev Each "Dogie" NFT will get it's `tokenId`, so everything will be unique
+    /// @dev unique nft is based on the contract address which basically represents the collection
+    /// and then the `tokenId`
+
+    /*/////////////////////////////////////////////////////////////////////////////
+                                    CONSTRUCTOR
+    /////////////////////////////////////////////////////////////////////////////*/
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    /*/////////////////////////////////////////////////////////////////////////////
+                                Public Functions
+    /////////////////////////////////////////////////////////////////////////////*/
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    /// URI - Uniform Resource Identifier
+    /// A Uniform Resource Identifier (URI) is a string of characters that identifies a resource on the internet.
+    /// URIs are used to identify web pages, files, and other resources.
+    /// @dev tokenURI is almost simliar to api endpoint hosted somewhere, it will return the `metadata` for the NFT
+
+    function tokenURI(
+        uint256 _tokenId
+    ) public view override returns (string memory) {
+        return "";
+    }
 }
